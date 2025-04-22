3cdf7955b935d8ec9927448aa3910e11920a36d9
Gabrielv024 <gabovar024@gmail.com>
2025-04-22 16:27:07 -0400

    ejercicios con for

---
+++ for.py
@@ -0,0 +1,68 @@
+# explicacion y uso de for
+#for i in range (5):
+# print("Hola")
+
+# for i in range(5):
+#    print(1+1)
+# cant=int(input("ingrese la cant de repeticiones "))
+
+# for i in range(cant):
+#     print("Repeticion" ,i+1)
+
+
+
+
+    # nombre=input("Ingrese su nombre")
+    # edad=input("Ingrese su edad")
+
+    # print("Hola", nombre, "su edad es ", edad)
+    # print(f"Hola {nombre} su edad es {edad}")
+
+# num=int(input("Ingrese un numero")) 
+#Escribir i,"x", j , "=", y*j
+# for i in range(1,11):
+    # print(num,"x", i "="i*num)
+
+    #Todas las tablas de multiplicar del 1 al 10
+
+    # for i in range(1,11)
+    # print("Tabla del ", i)
+    # for j in range(1,11):
+    #     print(i,"x",j,"=",i*j)
+    
+    #Pedir al usuario cant de notas y promediarlas
+
+# cant=int(input("Ingrese la cantidad de notas"))
+# total=0
+# notasAzules=0
+# for i in range(cant):
+#     print("Ingrese la nota ", i+1)
+#     nota=float(input())
+#     total=total+nota
+#     if nota>=4:
+#         notasAzules=notasAzules+1
+# prom=total/cant
+
+# print(f"Su promedio es {round(prom,1)}")
+# print(f"La cantidad de notas sobre 4 fue {notasAzules}")
+
+# if prom>=4:
+#     print("El alumno aprobo")
+# else:
+#     print("El alumno reprobo")
+
+clave=3344
+
+password=int(input("Ingrese las password :"))
+if password==clave:
+    print("Bienvenido al sistema")
+else:
+    print("Error, clave invalida")
+
+    for i in range(3):
+        password=int(input("Ingrese las password :"))
+if password==clave:
+    print("Bienvenido al sistema")
+    break
+else:
+    print("Error, clave invalida")
