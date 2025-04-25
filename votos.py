236bf41e7c0d62ae6d45dd564e6182925b7561ce
Gabrielv024 <gabovar024@gmail.com>
2025-04-25 18:45:13 -0400

    votos

---
+++ votos.py
@@ -0,0 +1,87 @@
+
+# (input("Ingrese la cantidad de votantes "))
+# Kaiser=int(0)
+# lol=int(0)
+# votos=int(0)
+# print("Por que postulante votara?")
+# print("Los postulantes son: Kaiser y lol")
+
+# (input("Por quien quiere votar"))
+
+# for kaiser>lol:
+#     print("Punto para Kaiser")
+# else
+
+# palabra=input("ingrese una pabalabra ")
+# caract=0
+# for i in palabra:
+#     print(i)
+#     caract=caract+1
+#     caract+1
+#     print("la cant de caracteres es", caract)
+# num=0
+# while num<=(5):
+#     print(num)
+#     num==5
+
+# resp=("no")
+
+# while resp!=("si")
+# resp=input("Desea salir del sistema?  :")
+
+# clave=3344
+# intentos=3
+# contraseña=int(input("Ingrese su contraseña"))
+
+# while clave!=contraseña :
+#     intentos-=1
+#     print("Quedan ", intentos, " intentos")
+#     print("Error, clave invalida")
+#     contraseña=int(input("Ingrese su contraseña"))
+#     if intentos==0:
+#         break
+
+# if clave==contraseña:
+#     print("Clave aceptada")
+# else:
+#     print("Sistema bloqueado")
+
+
+#pida al usuario nmeros infinitos y muestre si son par o impar
+#para salir ponga 0
+
+# num=5
+
+# while num!=0:
+#    num=int(input("Ingrese un numero, cero para salir")) 
+# if num % 2==0:
+#    print(f"el numero {num} es par")
+# else:
+#    print(f"el numero {num} es impar")
+
+# num=5
+
+# while num!=0:
+# num=int(input("Ingrese un numero, cero para salir"))
+# print("añadio el numero", num)
+# suma=num+num
+# print("la suma de todos sus numeros es ", suma)
+
+# print("/"100)
+
+# import random
+# randy=random.randint(1,10)
+# print(randy)
+
+#asignar 2 peleadores cada uno con 50 HP
+#peleas por turnos, cada uno ataca entre 1 y 10
+#gana el que quita todo el HP al contrincante
+
+
+
+
+
+
+
+
+
